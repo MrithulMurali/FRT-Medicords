2fcdbc26d19480aa68ae46df98be73b18a3eb8e7
MrithulMurali <imrithulmurali@gmail.com>
2021-12-03 13:23:30 +0530

    clean up

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -35,9 +35,12 @@ export default function Header() {
     if (choice.toLowerCase() === "y") {
       const token = localStorage.getItem("x-access-token");
       axios
-        .delete(`http://localhost:4000/api/delete/${key}`, {
-          headers: { "x-access-token": token },
-        })
+        .delete(
+          `http://medicords.eastus.cloudapp.azure.com:4000/api/delete/${key}`,
+          {
+            headers: { "x-access-token": token },
+          }
+        )
         .then((response) => {
           console.log(response.data);
           alert("Account deleted successfully! Thank you for using medicords!");
